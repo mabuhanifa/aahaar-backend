c50caf4c81dac32780adace2e531a3331fa30f75
mabuhanifa <moh.abuhanifa@gmail.com>
2024-12-02 18:52:52 +0600

    donor module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { CatsModule } from './modules/cats/cats.module';
+import { DonorModule } from './modules/donor/donor.module';
 
 @Module({
   imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CatsModule, DonorModule],
 })
 export class AppModule {}
