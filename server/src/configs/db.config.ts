140fcb1d69a81e46f958e9ecde63f383a64d3dd4
mabuhanifa <moh.abuhanifa@gmail.com>
2025-03-29 21:24:16 +0600

    db config imported to app

--- server/src/configs/db.config.ts
+++ server/src/configs/db.config.ts
@@ -1,23 +1,28 @@
-const db = {
-  dbPrefix: process.env.DB_PREFIX || 'mongodb+srv://',
-  dbUser: process.env.DB_USER || 'root',
-  dbHost: process.env.DB_HOST || 'localhost',
-  dbPort: parseInt(process.env.DB_PORT, 10) || 27017,
-  dbName: process.env.DB_NAME || 'test',
-  password: process.env.DB_PASSWORD || 'secret',
+// const db = {
+//   dbPrefix: process.env.DB_PREFIX || 'mongodb+srv://',
+//   dbUser: process.env.DB_USER || 'root',
+//   dbHost: process.env.DB_HOST || 'localhost',
+//   dbPort: parseInt(process.env.DB_PORT, 10) || 27017,
+//   dbName: process.env.DB_NAME || 'test',
+//   password: process.env.DB_PASSWORD || 'secret',
+// };
+
+// let uri: string;
+
+// if (process.env.DB_URL) {
+//   uri = process.env.DB_URL;
+// } else if (db.dbPrefix.includes('+srv://')) {
+//   uri = `${db.dbPrefix}${db.dbUser}:${db.password}@${db.dbHost}/${db.dbName}`;
+// } else {
+//   uri = `${db.dbPrefix}${db.dbUser}:${db.password}@${db.dbHost}:${db.dbPort}/${db.dbName}`;
+// }
+// export const MongoConfig = () => ({
+//   uri,
+// });
+
+// export default MongoConfig;
+
+const dbConfig = {
+  uri: `${process.env.MONGO_URI}/${process.env.MONGO_DB}` || 'localhost',
 };
-
-let uri: string;
-
-if (process.env.DB_URL) {
-  uri = process.env.DB_URL;
-} else if (db.dbPrefix.includes('+srv://')) {
-  uri = `${db.dbPrefix}${db.dbUser}:${db.password}@${db.dbHost}/${db.dbName}`;
-} else {
-  uri = `${db.dbPrefix}${db.dbUser}:${db.password}@${db.dbHost}:${db.dbPort}/${db.dbName}`;
-}
-export const MongoConfig = () => ({
-  uri,
-});
-
-export default MongoConfig;
+export default dbConfig;
