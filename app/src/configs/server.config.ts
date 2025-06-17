e4e99a27836c68a7a8d7d27fda78ae2ecf28fa5c
mabuhanifa <moh.abuhanifa@gmail.com>
2025-06-17 20:32:29 +0600

    previous app cleaned

--- app/src/configs/server.config.ts
+++
@@ -1,8 +0,0 @@
-const serverConfig = {
-  host: process.env.HOST || 'localhost',
-  port: (process.env.PORT as unknown as number) || 2023,
-  globalPrefix: 'api',
-  swaggerPrefix: 'api/docs',
-};
-
-export default serverConfig;
--- app/src/modules/donor/dto/update-donor.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDonorDto } from './create-donor.dto';
-
-export class UpdateDonorDto extends PartialType(CreateDonorDto) {}
